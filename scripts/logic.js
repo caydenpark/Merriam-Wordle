d632688997d53f5374967656bb14a1f6f2e43826
Cayden <114540789+caydenpark@users.noreply.github.com>
2023-10-13 16:15:51 -0600

    Game is officially playable!!!

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,31 +1,52 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Your code here, including the createBoard function
-    createBoard();
+let wotd;
+let wotdLength;
+
+async function fetchWOTD() {
+    // Get Word of the Day (wotd)
+    try {
+        const response = await fetch("https://corsproxy.io/?https://www.merriam-webster.com/word-of-the-day");
+        if (response.status === 200) {
+            const htmlContent = await response.text();
+            const tempDiv = document.createElement("div");
+            tempDiv.innerHTML = htmlContent;
+            wotd = tempDiv.querySelector(".word-header-txt").innerText.toUpperCase();
+            // wotd = "Block".toUpperCase();
+            if (tempDiv.querySelector(".word-header-txt")) {
+                // The fetch is complete, and wotd has been set
+                console.log("Merriam-Webster's WOTD: ", wotd);
+                wotdLength = wotd.length;
+                createBoard();
+            } else {
+                console.error("Failed to find .word-header-txt element.");
+            }
+        } else {
+            throw new Error("Failed to retrieve page content.");
+        }
+    } catch (error) {
+        console.error("Error: ", error);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Call the fetchWOTD function when the DOM is ready
+    fetchWOTD();
 });
 
 function createBoard() {
     /* 
-        CREATE THE BOARD 
-        Creates the board based off how long the word of the day is
+        Create the board based off how long the word of the day is
     */
     
-    // Get Word of the Day (wotd)
-    const wotd = "Block";
-    const wotdLength = wotd.length;
-    const numCols = wotdLength;
-
-    // Get container element in which the rows will go
-    const container = document.getElementById('board');
-    
     // Set the gridTemplateColumns property for all .row elements within #board container
+    const container = document.getElementById('board');
     const rows = container.querySelectorAll('.row');
     rows.forEach(row => {
         row.style.gridTemplateColumns = `repeat(${wotdLength}, auto)`;
     });
-
+    
     // Define number of rows and columns
+    const numCols = wotdLength;    
     let numRows = 6;
-
     if (wotdLength != 5) {
         if (wotdLength < 5) {
             numRows = 5;
@@ -44,10 +65,11 @@ function createBoard() {
     for (let i = 0; i < numRows; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
-    
-        // Adjusts width, height, and fontSize for each cell
+        
+        // Adjusts width, height, and fontSize for each tile
         for (let j = 0; j < numCols; j++) {
             const col = document.createElement('div');
+            col.id = `${i}-${j}`; // Add unique ID to each tile
             if (wotdLength > 5) {
                 if (wotdLength == 6) {
                     col.style.width = "45px";
@@ -108,5 +130,83 @@ function createBoard() {
         }
         container.appendChild(row);
     }
-}
 
+    let currentRow = 0;
+    let currentTile = 0;
+    const wotdLetters = wotd.split('');
+    let guessLetters = [];
+    let guessWord;
+
+// Add event listeners to keyboard keys
+const keyboard = document.getElementById('keyboard');
+keyboard.addEventListener('click', (e) => {
+    const clickedKey = e.target;
+    if (clickedKey.id !== 'backspace' && clickedKey.id !== 'enter') {
+        // Populate the next empty tile with the clicked key's letter
+        const tileId = `${currentRow}-${currentTile}`;
+        console.log(`tileID: ${tileId}`);
+        const tile = document.getElementById(tileId);
+        tile.textContent = clickedKey.textContent;
+        currentTile++;
+        guessLetters.push(clickedKey.textContent);
+        guessWord = guessLetters.join("");
+        console.log(guessWord);
+    } else if (clickedKey.id == 'backspace') {
+        // Handle backspace key: clear the content of the last populated tile
+        if (currentTile > 0) {
+            currentTile--;
+            const tileId = `${currentRow}-${currentTile}`;
+            console.log(`tileID: ${tileId}`);
+            const tile = document.getElementById(tileId);
+            tile.textContent = '';
+            guessLetters.pop();
+            guessWord = guessLetters.join("");
+            console.log(guessWord);
+        }
+    } else if (clickedKey.id == 'enter') {
+        if (wotdLength == guessWord.length) {
+            let remainingLetters = wotd.split('');
+            for (let i = 0; i < wotdLength; i++) {
+                const tileId = `${currentRow}-${i}`;
+                console.log(`tileID: ${tileId}`);
+                const tile = document.getElementById(tileId);
+                if (guessLetters[i] === wotdLetters[i]) {
+                    const letterToRemove = guessLetters[i];
+                    const indexToRemove = remainingLetters.indexOf(letterToRemove);
+                    if (indexToRemove !== -1) {
+                        remainingLetters.splice(indexToRemove, 1);
+                    }
+                    console.log(`tileID: ${tileId}`);
+                    console.log(`tile: ${tile}`);
+                    
+                    tile.classList.add('green-tile');
+                } else {
+                    tile.classList.add('gray-tile');
+                }
+                console.log(`Remaining Letters: ${remainingLetters}`);
+            }
+            for (let i = 0; i < wotdLength; i++) {
+                const tileId = `${currentRow}-${i}`;
+                console.log(`tileID: ${tileId}`);
+                const tile = document.getElementById(tileId);
+                if (remainingLetters.includes(guessLetters[i]) && guessLetters[i] !== wotdLetters[i]) {
+                    const letterToRemove = guessLetters[i];
+                    const indexToRemove = remainingLetters.indexOf(letterToRemove);
+                    if (indexToRemove !== -1) {
+                        remainingLetters.splice(indexToRemove, 1);
+                    }
+                    tile.classList.add('yellow-tile');
+                }
+                console.log(`Remaining Letters: ${remainingLetters}`);
+            }
+        } else {
+            console.error("User's guess not same length as WOTD");
+        }
+        // Reset guessWord and guessLetters every time enter is pressed
+        guessWord = "";
+        guessLetters = [];
+        currentRow++;
+        currentTile = 0;
+    }
+});
+}
